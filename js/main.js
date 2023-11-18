64876a3f5925d8c36f17efbfc0c2ca4601d393c2
Tharindu Ishan <ishanthari96@gmail.com>
2023-11-18 11:19:26 +0530

    Make jump animation

--- js/main.js
+++ js/main.js
@@ -2,15 +2,15 @@ const divElm=document.querySelector('div');
 divElm.style.top=`${innerHeight-divElm.offsetHeight-100}px`;
 divElm.style.backgroundSize='cover';
 
-let run=false;
+let run=true;
 let jump=false;
 let dx=0;
 let dy=0;
 addEventListener('keydown',(e)=>{
-    if(e.key==='ArrowRight'){
-        run=true;
-        dx=10;
-    }
+    // if(e.key==='ArrowRight'){
+    //     run=true;
+    //     dx=10;
+    // }
    
 });
 addEventListener('keydown',(e)=>{
@@ -21,51 +21,76 @@ addEventListener('keydown',(e)=>{
 });
 addEventListener('keyup',(e)=>{
     if(e.key==='ArrowRight'||e.key=='ArrowUp'){
-        run=false;
-        jump=false;
+        // run=false;
+        // jump=false;
         dx=0;
     }
 });
-let index=0;
+let index=1;
 function renderDino(){
     if(run){
         if(index>8) index=1;
         divElm.style.backgroundImage=`url('dino/Walk (${index++}).png')`;
 
     }
-    else if(jump){
+    // else if(jump){
+
+    //     if(index>12) index=1;
+    //     divElm.style.backgroundImage=`url('dino/Jump (${index++}).png')`;
+
+    // }
+    // else{
+    //     if(index>10) index=1;
+    //     divElm.style.backgroundImage=`url('dino/Idle (${index++}).png')`;
+    // }
+
+}
+function runDino(){
+     if(jump){
+        clearInterval(renderDino);
         if(index>12) index=1;
         divElm.style.backgroundImage=`url('dino/Jump (${index++}).png')`;
 
-    }
-    else{
-        if(index>10) index=1;
-        divElm.style.backgroundImage=`url('dino/Idle (${index++}).png')`;
-    }
 
-}
-function handleJump(){
-    if(jump){
-        divElm.style.top=`${divElm.offsetTop-dy}px`
     }
 }
-function handleJump2(){
-    if(jump){
-        divElm.style.top=`${divElm.offsetTop+dy}px`
-    }
-}
-setInterval(renderDino,1000/21);
-setInterval(handleJump,50);
-setInterval(handleJump2,50);
+
 
 let backgroundImagePositionX=0;
+let dinoTop=`${innerHeight-divElm.offsetHeight-100}`;
+console.log(dinoTop);
 
 function moveBackground(){
-
+    
     if(run){
-
+        
         backgroundImagePositionX-=20;
         document.querySelector('body').style.backgroundPositionX=backgroundImagePositionX+'px'
     }
+}
+let i=true;
+function jumpRino(){
+    if(jump){
+        
+
+        if(dinoTop>0 && i){
+
+            dinoTop-=20;
+            console.log(dinoTop);
+            divElm.style.top=dinoTop+'px';
+        }else{
+            i=false;
+            dinoTop+=20;
+            divElm.style.top=dinoTop+'px';
+            if(dinoTop===369) {
+                jump=false;
+                i=true;
+            }
+        }
     }
+}
+
+setInterval(renderDino,100);
+setInterval(runDino,100);
 setInterval(moveBackground,50);
+setInterval(jumpRino,10);
